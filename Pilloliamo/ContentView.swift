f5ce4da7231723244817d56f40e4ff9f8ed727be
Raffaele Mosca <raffaele.mosca01@icloud.com>
2022-04-09 12:55:54 +0200

    Initial Commit

---
+++ Pilloliamo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pilloliamo
+//
+//  Created by Raffaele Mosca on 09/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pilloliamo/PilloliamoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PilloliamoApp.swift
+//  Pilloliamo
+//
+//  Created by Raffaele Mosca on 09/04/22.
+//
+
+import SwiftUI
+
+@main
+struct PilloliamoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
